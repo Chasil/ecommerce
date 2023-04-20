f649c915f13d40cda1303596ea2f0452dff8010e
mwojcikbs <mwojcikbs@gmail.com>
2023-04-20 09:06:29 +0200

    add product to homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(
+        ManagerRegistry $doctrine
+    ): Response
     {
+        $productRepository = $doctrine->getRepository(Product::class);
+        $products = $productRepository->findAll();
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'products' => $products
         ]);
     }
 }
